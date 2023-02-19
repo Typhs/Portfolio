2cff881c59b6f7facd7bc6e4fefbc668072b34c0
Typhs <tailanmorita@hotmail.com>
2023-02-19 15:50:18 -0300

    Removed eslint 'multi-word component' error

---
+++ src/views/TestView.vue
@@ -0,0 +1,30 @@
+<template>
+	<div>
+		<typewriter/>
+	</div>
+</template>
+
+<script>
+import Typewriter from "@/components/Typewriter.vue";
+
+export default {
+	components: {
+		Typewriter,
+	},
+	data() {
+		return {
+			
+		}
+	},
+	mounted () {
+		
+	},
+	methods: {
+		
+	},
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
