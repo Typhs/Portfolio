305950052c9642251f1c9cb91ae1978c7d58a051
Typhs <tailanmorita@hotmail.com>
2023-03-19 15:25:14 -0300

    Created paragraph transition of letters floating into position - looks pretty solid

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,10 +1,28 @@
 <template>
-	<div>
-		test
+	<div class="">
+
+		<div class="words-container" ref="words-container">
+			
+			<div v-for="(word, idx) in paragraphSplit" :key="'word-'+ idx" class="word"
+				v-html="word"
+			>
+				<!-- <span>{{word}}</span> -->
+			</div>
+		</div>
+
+		<div v-html="banana">
+
+		</div>
+
 	</div>
 </template>
 
 <script>
+import anime from 'animejs';
+
+
+
+
 
 export default {
 	components: {
@@ -12,14 +30,47 @@ export default {
 	},
 	data() {
 		return {
-		
+			paragraph: "I am Ty, and Welcome to my portfolio!. I am a Front-End developer specialized in putting out great products of relatively high complexity with little directional information, producing both the design and mechanical interactions of visual assets.",
+			banana: "<h1>coolz </h1>"
     }
 	},
 	mounted () {
-    
+		this.init()
+	},
+	computed: {
+		paragraphSplit() {
+			let r = structuredClone(this.paragraph)
+			r = r.split("")
+			let idx = 0
+			r.forEach(letter => {
+				if (letter == " "){
+					r[idx] = "&nbsp;"
+				}
+				idx = idx + 1
+			});
+
+			return r 
+		}
 	},
 	methods: {
-    
+		init(){
+			const CONTAINER_HEIGHT = this.$refs['words-container'].clientHeight
+			const CONTAINER_WIDTH = this.$refs['words-container'].clientWidth
+			
+			anime({
+				targets: ".word",
+				direction: "reverse",
+				position: ["relative", "absolute"],
+				duration: () => anime.random(1000, 3000) + 500,
+				left: () => anime.random(0, CONTAINER_WIDTH) + "px", 
+				top: () => anime.random(0, CONTAINER_HEIGHT) + "px", 
+				rotate: () => anime.random(-180, 180),
+				easing: "easeInOutQuad",
+				opacity: [1, 0],
+				delay: anime.stagger(130, {from: 'last'}),
+			})
+
+		}
 	},
 }
 
@@ -27,4 +78,22 @@ export default {
 
 <style lang="scss" scoped>
 
+.words-container{
+	width: 800px;
+	height: 600px;
+	margin: 20px auto auto auto;
+	border: 2px solid grey;
+	align-items: flex-start;
+	align-content: start;
+	display: flex;
+	flex-wrap: wrap;
+	position: relative;
+	overflow: hidden;
+	padding: 20px;
+}
+.word{
+	//outline: 1px solid red;
+	//margin-right: 5px;
+	position: relative;
+}
 </style>
