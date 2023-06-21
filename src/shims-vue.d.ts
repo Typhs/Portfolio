8a98921c7ea5e1d402998579bd5a9bff43099123
Typhs <tailanmorita@hotmail.com>
2023-06-20 21:28:58 -0300

    added Shim to allow '' with Vue Options API

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,5 +1,16 @@
+import * as runtimeCore from '@vue/runtime-core'
+
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
   export default component
+}
+
+declare module '@vue/runtime-core' {
+  interface ComponentCustomProperties {
+    $refs: {
+      [key: string]: HTMLElement|any,
+    },
+    // ... more stuff
+  }
 }
