1098c6efc2c13a1fee3791777e64a764a2ffacd2
Typhs <tailanmorita@hotmail.com>
2023-07-29 15:33:27 -0300

    sped up speedUp rate on mouseMove Circle Header

--- src/views/ViewComponents/Home/SkyHeader.vue
+++ src/views/ViewComponents/Home/SkyHeader.vue
@@ -61,7 +61,7 @@ export default {
 	methods: {
 		speedUpConstellation(){
       clearTimeout(this.speedTimeout)
-      this.constellationSpeed = 3
+      this.constellationSpeed = 15
       this.speedTimeout = setTimeout(() => {
         this.constellationSpeed = 1;  
       }, 100);
