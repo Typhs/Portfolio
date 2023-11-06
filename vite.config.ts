43e43d23e7cf7e65cd948117fbbe96859fdfa28c
Typhs <tailanmorita@hotmail.com>
2023-11-05 21:49:49 -0300

    setup viteconfig base

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/portfolio-v2/",
   plugins: [
     vue({
       template: { transformAssetUrls },
