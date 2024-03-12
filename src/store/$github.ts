6d94cdcf8d39d5c4dbd90e2d6e6c66071476a969
Typhs <tailanmorita@hotmail.com>
2024-03-11 22:34:12 -0300

    added cashing system in github store

--- src/store/$github.ts
+++ src/store/$github.ts
@@ -1,9 +1,10 @@
 import { defineStore } from "pinia";
 import axios from "axios";
+import { resolve } from "path";
 
 export const use$github = defineStore("$github", {
   state: () => ({
-    directorMode: {},
+    cashedCodes: {} as { [key: string]: string },
   }),
   actions: {
     async fetchGitCode(path: string) {
@@ -11,13 +12,19 @@ export const use$github = defineStore("$github", {
         Accept: "application/vnd.github.v3.raw",
       };
 
+      // if the code is already cashed, return it
+      if (this.cashedCodes[path]) {
+        return new Promise((resolve) => resolve(this.cashedCodes[path]));
+      }
+
       await new Promise((resolve) => setTimeout(resolve, 1000));
-      // timeout to smooth out user experience
-      // it's way too fast without the timeout
+      // timeout to smooth out user experience and show off our sick loader
+      // it's way too fast without it
 
       return axios
         .get(import.meta.env.VITE_GITHUB_API_PATH + path, { headers })
         .then((res) => {
+          this.cashedCodes[path] = res.data;
           return res.data;
         });
     },
