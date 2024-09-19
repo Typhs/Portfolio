5ff8a4d28db897d67061d9c63a634791450849b0
Typhs <tailanmorita@hotmail.com>
2024-09-19 01:21:24 -0300

    added Microsoft Clarity setup

--- src/functions/AppStartedScript.ts
+++ src/functions/AppStartedScript.ts
@@ -43,4 +43,21 @@ export default function () {
   // ========= on before window close =========
   window.onbeforeunload = () => {};
   // ========= on before window close =========
+
+  // ========= MICROSOFT CLARITY =========
+  if (process.env.NODE_ENV != "development") {
+    (function (c: any, l: any, a: any, r: any, i: any, t?: any, y?: any) {
+      c[a] =
+        c[a] ||
+        function () {
+          (c[a].q = c[a].q || []).push(arguments);
+        };
+      t = l.createElement(r);
+      t.async = 1;
+      t.src = "https://www.clarity.ms/tag/" + i;
+      y = l.getElementsByTagName(r)[0];
+      y.parentNode.insertBefore(t, y);
+    })(window, document, "clarity", "script", "o5h89udm3k");
+  }
+  // ========= MICROSOFT CLARITY =========
 }
